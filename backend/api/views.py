ec61fd9707ee9de905c733e495d1d98e10322c8d
volpicelli <ca.volpicelli@gmail.com>
2024-12-05 09:39:50 +0100

    Assegnato a cantiere error

--- backend/api/views.py
+++ backend/api/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework import generics
 from django.core import serializers
 from django.core.exceptions import ObjectDoesNotExist
+from rest_framework import exceptions
 
 from .articoli_serializer import Articoliserializer
 from .azienda_serializer import Aziendaserializer
@@ -34,6 +35,34 @@ from home.models import Cantiere,Articoli,Azienda,Cliente,Fatture,Fornitori,Ordi
 class Assegnato_CantiereList(generics.ListCreateAPIView):
     queryset = Assegnato_Cantiere.objects.all()
     serializer_class = Assegnato_CantiereSerializer
+
+    def handle_exception(self, exc):
+        """
+        Handle any exception that occurs, by returning an appropriate response,
+        or re-raising the error.
+        """
+        if isinstance(exc, (exceptions.NotAuthenticated,
+                            exceptions.AuthenticationFailed)):
+            # WWW-Authenticate header for 401 responses, else coerce to 403
+            auth_header = self.get_authenticate_header(self.request)
+
+            if auth_header:
+                exc.auth_header = auth_header
+            else:
+                exc.status_code = status.HTTP_403_FORBIDDEN
+
+        exception_handler = self.get_exception_handler()
+
+        context = self.get_exception_handler_context()
+        #context="POLLO"
+        response = exception_handler(exc, context)
+
+        if response is None:
+            self.raise_uncaught_exception(exc)
+
+        response.exception = True
+        response.data["errors"]="Personale gia assegnato al cantiere"
+        return response
 
 
 class Assegnato_CantiereDetail(generics.RetrieveUpdateDestroyAPIView):
