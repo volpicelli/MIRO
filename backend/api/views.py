1fef227956c94ecd629af4e9124523edffdecbf1
volpicelli <ca.volpicelli@gmail.com>
2024-12-09 11:34:31 +0100

    Update fix

--- backend/api/views.py
+++ backend/api/views.py
@@ -318,6 +318,19 @@ class ClienteDetail(generics.RetrieveUpdateDestroyAPIView):
         object = Cliente.objects.get(pk=pk) #kwargs['pk'])
         serializer = self.serializer_class(object)
         return Response(serializer.data)
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
  
 class FattureList(generics.ListCreateAPIView):
     queryset = Fatture.objects.all()
@@ -339,6 +352,7 @@ class FattureDetail(generics.RetrieveUpdateDestroyAPIView):
         #pk = self.kwargs.get('pk')
         object = Fatture.objects.get(pk=pk) #kwargs['pk'])
         serializer = self.serializer_class(object)
+        
         return Response(serializer.data)
  
 class FornitoriList(generics.ListCreateAPIView):
